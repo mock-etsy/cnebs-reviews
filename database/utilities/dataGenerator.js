5c6040eb8e19cad410ff2272aaad17d8f3cf13d1
Charles <charles.neblett@gmail.com>
2019-07-10 18:09:32 -0500

    Cleaned up database utilities

--- database/utilities/dataGenerator.js
+++ database/utilities/dataGenerator.js
@@ -1,39 +1,6 @@
 const faker = require('faker');
 const uuid = require('uuid/v4');
-const weightedRandom = require('./weightedRandomNumber');
-
-// Helper used to generate a set of 100 fake reviews with average rating
-const reviewGen = () => {
-  let total = 0
-  
-  const reviewArr = new Array(100).fill(0);
-  
-  for (let review = 0; review < reviewArr.length; review++) { 
-    reviewArr[review] = {
-      reviewId: uuid(),
-      name:     faker.name.findName(),
-      date:     faker.date.past(1),
-      rating:   Number(weightedRandom.weightedRandom( { 5:0.5, 4:0.2, 3:0.1, 2:0.1, 1:0.1} )),
-      review:   faker.lorem.paragraph(4)
-    }
-  };
-  
-  for (let review = 0; review < reviewArr.length; review++) {
-    total += reviewArr[review].rating;
-  };
-  
-  let average = Math.floor(total/100);
-
-  console.log('Total: ', total);
-  console.log('Average: ', average);
-  
-  const reviewObj = {
-    averageRating: average,
-    reviews:       reviewArr
-  };
-
-  return reviewObj;
-}
+const reviewGen = require('./reviewGenerator');
 
 // Helper to generate a set of sellers with respective review datasets
 const dataGen = () => {
@@ -43,11 +10,11 @@ const dataGen = () => {
     sellerArr[seller] = {
       sellerName:    faker.name.findName(),
       sellerId:      uuid(),
-      reviewInfo:    reviewGen()
+      reviewInfo:    reviewGen.reviewGen()
     }
     count++
   }
-  console.log('Seller count:', count)
+  // console.log('Seller count:', count)
   return sellerArr;
 }
 
--- database/utilities/weightedRandomNumber.js
+++ database/utilities/weightedRandomNumber.js
@@ -1,3 +1,6 @@
+
+// helper to generate a weighted random number from a range of values.
+// ex -> weightedRandom( { 1:0.5, 2:0.3, 3:0.2 } ) ... 50% chance of returning 1, 30% of 2, 20% of 3
 weightedRandom = (spec) => {
   let i,
   sum=0, 
