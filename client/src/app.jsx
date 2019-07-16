a7d519ab24386368035aa3e4ad6e48b71b71826f
Charles <charles.neblett@gmail.com>
2019-07-16 12:30:11 -0500

    Renders reviews for data given by message bus OR randomly generated reviews

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -85,75 +85,70 @@ class App extends React.Component {
 
   // Retrieve and use randomly chosen seller on refresh
   componentDidMount() {
-
-    // this.reviewChannel.onmessage = (e) => {
-    //   this.retrieveSeller(e.data);
-    // }
-
-    // axios
-    //   .get('/reviews/sellers')
-    //   .then( res => {
-    //     const ids  = [];
-    //     const data = res.data
-    //     data.forEach( id => ids.push(id.sellerID))
-    //     let randomId = ids[Math.floor(Math.random() * (101 - 1)) + 1]
-    //     this.retrieveSeller(randomId);
-    //   })
-    //   .catch( err => console.log(`Error retrieving seller IDs from DB`));
+    axios
+      .get('/reviews/sellers')
+      .then( res => {
+        const ids  = [];
+        const data = res.data
+        data.forEach( id => ids.push(id.sellerID))
+        let randomId = ids[Math.floor(Math.random() * (101 - 1)) + 1]
+        this.retrieveSeller(randomId);
+      })
+      .catch( err => console.log(`Error retrieving seller IDs from DB`));
   }
 
   render() {
-    this.reviewChannel.onmessage = function(e) {
-      console.log('Received: ', e.data);
+    this.reviewChannel.onmessage = (e) => {
       this.retrieveSeller(e.data);
     }
 
     console.log(`Rendering reviews for: \n
                  Seller:   ${this.state.currentSeller}\n
-                 SellerID: ${this.state.currentSellerID}`)
+                 SellerID: ${this.state.currentSellerID}\n
+                 Product:  ${this.state.currentReviews[0].productTitle}`)
 
     return (
-    <Container>
-      <Col>
-        <span className='reviewsHeader'>Reviews 
-          <span className='averageStars'>
-            <Average
-              averageRating={this.state.currentAverageRating}
-              totalReviews={this.state.currentReviews.length}
-            />
+      <Container>
+        <Col>
+          <span className='reviewsHeader'>Reviews 
+            <span className='averageStars'>
+              <Average
+                averageRating={this.state.currentAverageRating}
+                totalReviews={this.state.currentReviews.length}
+              />
+            </span>
           </span>
-        </span>
-      </Col>
-      <Row>
-        <Col>
-          <MasterReviewList 
-            currentReviews     ={this.state.currentReviews}
-            moreClicked        ={this.state.moreClicked}
-            readAllClicked     ={this.state.readAllClicked}
-            handleReadAllClick ={this.handleReadAllReviewsClick}
-          />
         </Col>
-      </Row>
-      <Row>
-        <Col>
-            {this.state.moreClicked === true ?
-            this.state.readAllClicked === true ? <span /> : 
-              <Button 
-                className ='readAllReviews' 
-                variant   ="dark" 
-                size      ="lg"
-                onClick   ={this.handleReadAllReviewsClick}>
-                  Read All Reviews ({this.state.currentReviews.length-11})
-              </Button> : 
-              <span 
-                className ='plusMore' 
-                onClick   ={this.handleMoreClick}>
-                  + More
-              </span>}
-        </Col>
-      </Row>
-      <Row><p /></Row>
-    </Container>
+        <Row>
+          <Col>
+            <MasterReviewList 
+              currentReviews     ={this.state.currentReviews}
+              moreClicked        ={this.state.moreClicked}
+              readAllClicked     ={this.state.readAllClicked}
+              handleReadAllClick ={this.handleReadAllReviewsClick}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+              {this.state.moreClicked === true ?
+              this.state.readAllClicked === true ? <span /> : 
+                <Button 
+                  className ='readAllReviews' 
+                  variant   ="dark" 
+                  size      ="lg"
+                  onClick   ={this.handleReadAllReviewsClick}>
+                    Read All Reviews ({this.state.currentReviews.length-11})
+                </Button> : 
+                <span 
+                  className ='plusMore' 
+                  onClick   ={this.handleMoreClick}>
+                    + More
+                </span>}
+          </Col>
+        </Row>
+        <Row><p /></Row>
+      </Container>
     );
   }
 
